46c3a894cbd6f88c0aaa7b50767969b1294c27a7
Garv Vohra <garv.vohra21@gmail.com>
2026-07-03 02:28:34 +1000

    Added about me section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
 import { TechBackground } from "@/components/TechBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <Navbar />
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
 
     </div>
